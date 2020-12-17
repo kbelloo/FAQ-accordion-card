540bb4772536e2e424fa033f533dd2b225d1553b
Khalid Bello <khalidbello279@gmail.com>
2020-12-17 23:01:46 +0100

    edit

--- faq-accordion-card-main/js/main.js
+++ faq-accordion-card-main/js/main.js
@@ -1,12 +1,16 @@
 let skipDouble = 0;
 
 
-function tryAgain(question, img){
+function changeFontImg(question, img){
     
     skipDouble++;
     if(skipDouble % 2 == 0){
     document.querySelector(question).classList.toggle("question-font-weight")
     document.querySelector(img).classList.toggle("rotate-img");
         
+        
     }
+    
 }
+
+

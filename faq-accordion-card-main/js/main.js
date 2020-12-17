3585918253e85d5084f6c5e4d1b182d398591993
Khalid Bello <khalidbello279@gmail.com>
2020-12-17 12:19:12 +0100

    completed

--- faq-accordion-card-main/js/main.js
+++ faq-accordion-card-main/js/main.js
@@ -1,15 +1,10 @@
-document.querySelectorAll(".question").forEach((question) => question.addEventListener("click", () => {
-    
-    if(question.parentNode.classList.contains("active")) {
-        
-        question.parentNode.classList.toggle("active");
-        
-    }else{
-        
-        document.querySelectorAll(".question").forEach(question => question.parentNode.classList.remove("active"));
-        
-        question.parentNode.classList.add("active");
+let skipDouble = 0;
+
+
+function tryAgain(question, img){
+    skipDouble++;
+    if(skipDouble % 2 == 0){
+    document.querySelector(question).classList.toggle("question-font-weight")
+    document.querySelector(img).classList.toggle("rotate-img");
     }
-    
-    
-}));
+}
